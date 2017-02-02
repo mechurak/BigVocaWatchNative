e613484929213834e2d0e8a20736490989b62e82
sshim from desktop <mechurak@gmail.com>
2017-02-02 23:27:40 +0900

    Initial commit

---
+++ inc/bigvocawatchnative.h
@@ -0,0 +1,18 @@
+#ifndef __bigvocawatchnative_H__
+#define __bigvocawatchnative_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "bigvocawatchnative"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.sshimnssso.bigvocawatchnative"
+#endif
+
+#endif /* __bigvocawatchnative_H__ */
